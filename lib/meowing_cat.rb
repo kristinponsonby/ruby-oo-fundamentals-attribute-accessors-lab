e0acb61621d1ee5d8d12c84936db6c9dbc0295d5
Kristin <kristinponsonby@gmail.com>
2021-02-18 13:16:43 -0500

    commit tree 4be5775f508aaf7e4da947b3dc68164a16c20108

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,17 @@
-## code your solution here. 
+class Cat  
+    attr_accessor :name, :meow 
+
+    def initialize 
+        @name = name
+        @meow = meow
+        puts "meow!"
+ end
+end
+
+
+
+ # Checklist
+ # {x} Create a new instance of the cat class
+ # {x} Cat class receives att_accessor 
+ # {x} Cat instance has a name
+ # {} Cat instance is able to meow. 
